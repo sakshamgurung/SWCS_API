d43477a1861202051f3a608d7d7c78abf7649d44
Saksham Gurung <be2016se504@gces.edu.np>
2021-03-23 08:07:16 +0545

    dotenv is used only for dev environment. March 23 2021

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,11 @@
 const dotenv = require('dotenv');
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-
-const envFound = dotenv.config();
-if(envFound.error){
-  throw new Error("Couldn't find .env file");
+if(process.env.NODE_ENV === "development"){
+  const envFound = dotenv.config();
+  if(envFound.error){
+    throw new Error("Couldn't find .env file");
+  }
 }
 
 module.exports = {
