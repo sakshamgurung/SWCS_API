948fc55c5732e6d697dcf94c7d4afebe1516d80f
Anjalbaral <anjalbaral7@gmail.com>
2021-04-10 09:06:02 +0545

    port changed

--- src/config/index.js
+++ src/config/index.js
@@ -1,22 +1,22 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
-if(process.env.NODE_ENV === "development"){
-  const envFound = dotenv.config();
-  if(envFound.error){
-    throw new Error("Couldn't find .env file");
-  }
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+if (process.env.NODE_ENV === "development") {
+	const envFound = dotenv.config();
+	if (envFound.error) {
+		throw new Error("Couldn't find .env file");
+	}
 }
+
 /**
  * if heroku error: Cannot read property 'split' of null
- * check env config variables and make sure database uri does not contain quotes in uri 
- * or 
- * you can set username and password as env config variables and use them with template literals in mongoose connect 
+ * check env config variables and make sure database uri does not contain quotes in uri
+ * or
+ * you can set username and password as env config variables and use them with template literals in mongoose connect
  * */
 
-
 module.exports = {
-  port: parseInt(process.env.PORT, 10),
-  databaseURL: process.env.MONGODB_URI,
-  remoteDatabaseURL: process.env.MONGODB_ATLAS_URI
+	port: parseInt(process.env.PORT, 10) || 5000,
+	databaseURL: process.env.MONGODB_URI,
+	remoteDatabaseURL: process.env.MONGODB_ATLAS_URI
 };
--- src/controllers/accountController.js
+++ src/controllers/accountController.js
@@ -1,29 +1,30 @@
-const {AccountServices} = require("../service/accountServices");
-const ApiError = require('../error/ApiError');
+const { AccountServices } = require("../service/accountServices");
+const ApiError = require("../error/ApiError");
 
-class AccountController{
-    async signUp(request, response, next){
-        try {
-            const { signUpData } = request.body;
-            const { role } = request.params;
+class AccountController {
+	async signUp(request, response, next) {
+		try {
+			const { signUpData } = request.body;
+			const { role } = request.params;
 
-            const accountServices = new AccountServices();
-            const result = await accountServices.signUp(role, signUpData);
-            response.json(result);
-        } catch (error) {
-            throw ApiError.serverError("Account Error: " + error.message);
-        }
-    }
-    async login(request, response, next){
-        try {
-            const { loginData } = request.body;
-            const { role } = request.params;
-            
-            const result = {loginStatus:"success"};
-            response.json(result);
-        } catch (error) {
-            throw ApiError.serverError("Account Error: " + error.message);
-        }
-    }
+			const accountServices = new AccountServices();
+			const result = await accountServices.signUp(role, signUpData);
+			response.json(result);
+		} catch (error) {
+			throw ApiError.serverError("Account Error: " + error.message);
+		}
+	}
+
+	async login(request, response, next) {
+		try {
+			const { loginData } = request.body;
+			const { role } = request.params;
+
+			const result = { loginStatus: "success" };
+			response.json(result);
+		} catch (error) {
+			throw ApiError.serverError("Account Error: " + error.message);
+		}
+	}
 }
 exports.AccountController = AccountController;
