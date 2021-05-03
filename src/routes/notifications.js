01304b57e86841aa5526f54b59e0ccfbb5a7da2c
Saksham Gurung <be2016se504@gces.edu.np>
2021-05-03 21:05:58 +0545

    minor modification

--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -1,23 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const util = require("../utilities/notificationUtil");
-const {
-	NotificationController,
-} = require("../controllers/notificationController");
+const { NotificationController } = require("../controllers/notificationController");
 const catchAsync = require("../error/catchAsync");
 
 const notificationController = new NotificationController();
-router.post("/notify", catchAsync(util.notify));
-router.get(
-	"/:role/:id/notifications",
-	catchAsync(notificationController.getAllNotification)
-); //role: company, staff, customer
-router.get(
-	"/notifications/:id",
-	catchAsync(notificationController.getNotificationById)
-);
-router.delete(
-	"/notifications/:id",
-	catchAsync(notificationController.deleteNotificationById)
-);
+//router.post("/notify", catchAsync(util.notify));
+router.get("/:role/:id/notifications", catchAsync(notificationController.getAllNotification)); //role: company, staff, customer
+router.get("/notifications/:id", catchAsync(notificationController.getNotificationById));
+router.delete("/notifications/:id", catchAsync(notificationController.deleteNotificationById));
 module.exports = router;
